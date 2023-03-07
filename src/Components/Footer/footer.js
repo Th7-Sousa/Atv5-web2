f9417e0d127a8050f8d7dbee3ad8098619fb2fdb
Th7-Sousa <thiagof.profissional@gmail.com>
2023-03-06 22:18:36 -0300

    ..

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,24 +1,25 @@
-import './styles.css';
+import "./styles.css";
 
-
-
-
-function Rodape(){
-    return(
-        <footer className="banner2">
-
-            {/* <img className='fund' src="./imagens/fundo.png" alt="nao encontrada a foto"/> */}
-            <div className='ico'>
-                <div><img className='ima'  src='./imagens/fb.png'/></div>
-                <div><img className='ima'  src='./imagens/tw.png'/></div>
-                <div><img className='ima'  src='./imagens/ig.png'/></div>
-            </div>
-            <div className='auto'>
-                <h4>Desenvolvido por André joás</h4>
-            </div>
-        </footer>
-
-    )
+function Rodape() {
+  return (
+    <footer className="banner2">
+      {/* <img className='fund' src="./imagens/fundo.png" alt="nao encontrada a foto"/> */}
+      <div className="ico">
+        <div>
+          <img className="ima" src="./imagens/fb.png" />
+        </div>
+        <div>
+          <img className="ima" src="./imagens/tw.png" />
+        </div>
+        <div>
+          <img className="ima" src="./imagens/ig.png" />
+        </div>
+      </div>
+      <div className="auto">
+        <h4>Desenvolvido por André joás e Th do front</h4>
+      </div>
+    </footer>
+  );
 }
 
 export default Rodape;
